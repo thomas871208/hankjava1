279191023913319b2cda32a707b7b0708954089f
Thomas <Thomas@Thomas_pc>
2018-12-12 21:38:19 +0800

    card

--- src/com/tom/Poker.java
+++ src/com/tom/Poker.java
@@ -25,7 +25,12 @@ public class Poker {
 		
 		public void  print(){ 
 				for (int i=0 ; i < 52; i++ ) {
+				if (i%13 == 12)	{
+					System.out.println(cards[i].get());
+				}
+				else {
 					System.out.print(cards[i].get());
+				}
 				/*char [] m = {0x2660,0x2665,0x2666,0x2663};
 				String [] n = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 				if (i%13 == 12){
